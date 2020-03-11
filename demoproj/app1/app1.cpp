6ff747f95fee0f32b0cb7e9e11ca0af930738088
jiahsuanlo <jiahsuanlo@yahoo.com>
2020-03-11 10:37:43 -0700

    add demoproj folder to demonstrate sub-directory usage

---
+++ demoproj/app1/app1.cpp
@@ -0,0 +1,23 @@
+﻿// demoproj.cpp : Defines the entry point for the application.
+//
+
+#include "app1.h"
+#include <lib1.h>
+#include <opencv2/core.hpp>
+#include <pcl/io/pcd_io.h>
+#include <config.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	cout << "8+5= " << add(8, 5) << "\n";
+
+	cv::Mat mat = cv::Mat::eye(2, 2, CV_8UC1);
+	cout << "mat= " << mat << "\n";
+
+	cout << "Version= " << VERSIONSTR << "\n";
+
+	return 0;
+}
---
+++ demoproj/lib1/lib1.cpp
@@ -0,0 +1,6 @@
+#include "lib1.h"
+
+int add(int a, int b)
+{
+	return a+b;
+}
