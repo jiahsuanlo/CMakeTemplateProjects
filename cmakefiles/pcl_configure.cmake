4dad31b74f6e6e8ae80b76c3db96b66b5eb7408d
joshlo <jiahsuan.lo@preact-tech.com>
2020-04-14 23:43:18 -0700

    update pcl configure file

--- cmakefiles/pcl_configure.cmake
+++ cmakefiles/pcl_configure.cmake
@@ -1,7 +1,14 @@
-cmake_minimum_required(VERSION 2.8 FATAL_ERROR)
+cmake_minimum_required(VERSION 3.12 FATAL_ERROR)
 
-find_package(PCL 1.8.1 REQUIRED)
+#find_package(PCL EXACT REQUIRED PATHS "$ENV{PCL_ROOT}") #COMPONENTS common io filters visualization)
+
+find_package(PCL 1.10.1)
 include_directories(${PCL_INCLUDE_DIRS})
 link_directories(${PCL_LIBRARY_DIRS})
 add_definitions(${PCL_DEFINITIONS})
 
+# === report PCL Information
+message([main] "PCL_ROOT= $ENV{PCL_ROOT}")
+message ([main] "PCL_DIR = ${PCL_DIR}")
+message ([main] "PCL_INCLUDE_DIRS = ${PCL_INCLUDE_DIRS}")
+message ([main] "PCL_LIBRARY_DIRS = ${PCL_LIBRARY_DIRS}")
