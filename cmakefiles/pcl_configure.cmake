80d82023eda85f7e33924058b94f688d8c9e90ec
joshlo <jiahsuan.lo@preact-tech.com>
2020-05-08 22:37:30 -0700

    update pcl library definition

--- cmakefiles/pcl_configure.cmake
+++ cmakefiles/pcl_configure.cmake
@@ -1,7 +1,5 @@
 cmake_minimum_required(VERSION 3.12 FATAL_ERROR)
 
-#find_package(PCL EXACT REQUIRED PATHS "$ENV{PCL_ROOT}") #COMPONENTS common io filters visualization)
-
 find_package(PCL 1.10.1)
 include_directories(${PCL_INCLUDE_DIRS})
 link_directories(${PCL_LIBRARY_DIRS})
@@ -12,3 +10,4 @@ message([main] "PCL_ROOT= $ENV{PCL_ROOT}")
 message ([main] "PCL_DIR = ${PCL_DIR}")
 message ([main] "PCL_INCLUDE_DIRS = ${PCL_INCLUDE_DIRS}")
 message ([main] "PCL_LIBRARY_DIRS = ${PCL_LIBRARY_DIRS}")
+message ([main] "PCL_LIBRARIES = ${PCL_LIBRARIES}")
